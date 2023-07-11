f4ebeae1503b0f16a89128618bfaf2f29ec16a69
abhidarbar116 <139134835+abhidarbar116@users.noreply.github.com>
2023-07-11 16:40:33 +0530

    added dark mode enable and disable button in navigation bar

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -41,24 +41,24 @@ export default function Textform(props) {
 
     return (
         <>
-            <div className="container my-3">
+            <div className="container my-3 t" style={{color: props.mode==="dark"?"white":"black"}} >
                 <h1> Text Summary</h1>
                 <p> {text.split(" ").filter(Boolean).length} word and {text.trim().length} characters</p>
                 <p> {0.008 * text.split(" ").filter(Boolean).length} Minutes To Read</p>
 
             </div>
-            <div className='container'>
+            <div className='container' style={{color: props.mode==="dark"?"white":"black"}}>
                 <div className="mb-3">
                     <h1>{props.heading}</h1>
 
-                    <textarea className="form-control" id="myBox"  onChange={handelOnChange} placeholder="Enter Text Here" rows="8"></textarea>
+                    <textarea className="form-control" id="myBox" style={{backgroundColor: props.mode==="light"?"white":"#A7C0C6",color: props.mode==="dark"?"white":"black"}} onChange={handelOnChange} placeholder="Enter Text Here" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary " onClick={handelOnClickButtonUpper}>Convert To Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={handelOnClickButtonLower}>Convert To Lowercase</button>
                 <button className="btn btn-primary " onClick={handelOnClickButtonFilterNumber}>Convert To Number</button>
                 <button className="btn btn-primary mx-2" onClick={handelOnClickButtonFilterString}>Convert To Stirng</button>
             </div>
-            <div  className="container my-3">
+            <div  className="container my-3" style={{color: props.mode==="dark"?"white":"black"}}>
                 <h2>Summary</h2>
                 <p>{summaryText}</p>
             </div>
